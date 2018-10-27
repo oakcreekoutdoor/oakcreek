d351e503861ab7473969169632f57e78b9738d03
jessicaqiu <jessicaelaineqiu@gmail.com>
2018-10-27 16:41:14 -0700

    Added Billing page HTML/CSS

---
+++ templates/billing.php
@@ -0,0 +1,70 @@
+<div id="billing" class="container">
+	<div class="shipping-info">
+		<h2>Shipping Information</h2>
+		<a href="#">Edit</a>
+		<div>
+			<p>Full Name</p>
+			<p>Address - City</p>
+			<p>Province/Country/Postal Code</p>
+		</div>
+	</div><!--shipping-info-->
+
+	<div class="billing-info">
+		<h2>Billing Information</h2>
+		<p>Step 2/3</p>
+		<input type="checkbox"> Use my shipping address as my billing address.<br>
+		<div>
+			<p>Full Name</p>
+			<p>Address - City</p>
+			<p>Province/Country/Postal Code</p>
+		</div>
+		<a class="btn prime" href="#">Continue to Payment</a>
+	</div><!--billing-info-->
+
+	<div class="cart-review">
+		<h2>Cart Review</h2>
+		<div class="cart-review-summary">
+			<div class="cart-review-item">
+				<img src="assets/bbq_toolset.jpg">
+				<div>
+					<p class="cart-label">Product Name</p>
+					<p class="cart-label">Qty: 1</p>
+					<p class="cart-label">Price</p>
+					<p>$140.00</p>
+				</div>
+			</div><!--cart-item-review-->
+			<div class="cart-review-item">
+				<img src="assets/bbq_toolset.jpg">
+				<div>
+					<p class="cart-label">Product Name</p>
+					<p class="cart-label">Qty: 1</p>
+					<p class="cart-label">Price</p>
+					<p>$140.00</p>
+				</div>
+			</div><!--cart-item-review-->
+		</div><!--cart-review--summary-->
+		<div>
+			<p class="cart-label">Subtotal</p>
+			<p>$280.00</p>
+			<p class="cart-label">Shipping</p>
+			<p>FREE</p>
+			<p class="cart-label">Coupon</p>
+			<p>$0.00</p>
+			<p class="cart-label">Tax</p>
+			<p>$7.26</p>
+			<div class="cart-total">
+				<p>Total</p>
+				<p>$287.26</p>
+			</div><!--cart-total-->
+		</div>
+	</div><!--cart-review-->
+
+	<div class="policies">
+		<p class="policies-title">Shipping & Delivery</p>
+		<p>We deliver to 17 countries with some restrictions. Terms & Conditions apply.</p>
+		<p class="policies-title">Free Returns</p>
+		<p>Free returns on items purchased within the last 60 days.
+		<p class="policies-title">Guarantee</p>
+		<p>Free returns on items purchased within the last 60 days.</p>
+	</div><!--policies-->
+</div><!--shipping-->
--- templates/pages.php
+++ templates/pages.php
@@ -1,10 +1,5 @@
 <?php
 	include("views/header.php");
-<<<<<<< HEAD
-	$content;
-	include("templates/shipping.php");
-=======
-	echo $content;
->>>>>>> 009182322da345a7a868ea64ef0117d1e9ff5b55
+	include("templates/billing.php");
 	include("views/footer.php");
 ?>
