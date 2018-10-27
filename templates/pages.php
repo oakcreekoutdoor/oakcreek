b38003af46bf0d06837c83069a466082515600db
jessicaqiu <jessicaelaineqiu@gmail.com>
2018-10-27 16:17:33 -0700

    Added Shipping page HTML/CSS

--- templates/pages.php
+++ templates/pages.php
@@ -1,5 +1,10 @@
 <?php
 	include("views/header.php");
+<<<<<<< HEAD
+	$content;
+	include("templates/shipping.php");
+=======
 	echo $content;
+>>>>>>> 009182322da345a7a868ea64ef0117d1e9ff5b55
 	include("views/footer.php");
 ?>
--- templates/shipping.php
+++ templates/shipping.php
@@ -14,33 +14,81 @@
 			<div>
 				<label>Alternative Phone</label>
 				<input type="text" name="altphone"/>
-			</div>
+			</div><!--formAltPhone-->
 			
-			<label>Address</label>
-			<input type="text"/>
+				<label>Address</label>
+				<input type="text"/>
+			
 			<div>
 				<label>Postal/Zip Code</label>
 				<input type="text" name="zipcode"/>
 			</div>
-			<div>
+			<div id="formCountry">
 				<label>Country</label>
 				<select>
 					<option>Select Country</option>
 				</select>
-			</div>
-			<div>
+			</div><!--formCountry-->
+			<div id="formProvince">
 				<label>State/Province</label>
 				<select>
 					<option>Select State/Province</option>
 				</select>
-			</div>
-			<div>
+			</div><!--formProvince-->
+			<div id="formCity">
 				<label>City</label>
 				<select>
 					<option>Select City</option>
 				</select>
-			</div>
+			</div><!--formCity-->
 			<a class="btn prime" href="#">Continue to Billing</a>
 		</form>
 	</div><!--shippingForm-->
+
+	<div class="cart-review">
+		<h2>Cart Review</h2>
+		<div class="cart-review-summary">
+			<div class="cart-review-item">
+				<img src="assets/bbq_toolset.jpg">
+				<div>
+					<p class="cart-label">Product Name</p>
+					<p class="cart-label">Qty: 1</p>
+					<p class="cart-label">Price</p>
+					<p>$140.00</p>
+				</div>
+			</div><!--cart-item-review-->
+			<div class="cart-review-item">
+				<img src="assets/bbq_toolset.jpg">
+				<div>
+					<p class="cart-label">Product Name</p>
+					<p class="cart-label">Qty: 1</p>
+					<p class="cart-label">Price</p>
+					<p>$140.00</p>
+				</div>
+			</div><!--cart-item-review-->
+		</div><!--cart-review--summary-->
+		<div>
+			<p class="cart-label">Subtotal</p>
+			<p>$280.00</p>
+			<p class="cart-label">Shipping</p>
+			<p>FREE</p>
+			<p class="cart-label">Coupon</p>
+			<p>$0.00</p>
+			<p class="cart-label">Tax</p>
+			<p>$7.26</p>
+			<div class="cart-total">
+				<p>Total</p>
+				<p>$287.26</p>
+			</div><!--cart-total-->
+		</div>
+	</div><!--cart-review-->
+
+	<div class="policies">
+		<p class="policies-title">Shipping & Delivery</p>
+		<p>We deliver to 17 countries with some restrictions. Terms & Conditions apply.</p>
+		<p class="policies-title">Free Returns</p>
+		<p>Free returns on items purchased within the last 60 days.
+		<p class="policies-title">Guarantee</p>
+		<p>Free returns on items purchased within the last 60 days.</p>
+	</div><!--policies-->
 </div><!--shipping-->
