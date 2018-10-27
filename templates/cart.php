1cd83a3752dfb68a64cfd97eb65851eb84434c63
jessicaqiu <jessicaelaineqiu@gmail.com>
2018-10-26 23:24:57 -0700

    Cart page HTML

---
+++ templates/cart.php
@@ -0,0 +1,72 @@
+<div id="cartSummary" class="container">
+	<h2>Cart Summary</h2>
+	<div id="cartItems">
+		<div class="cart-item">
+			<img src="assets/bbq_toolset.jpg">
+			<p class="cart-label">Product Name</p>
+			<div>
+				<p class="cart-label">Qty</p>
+				<input type="number" name="quantity" value="1" min="1">
+			</div>
+			<div>
+				<p class="cart-label">Remove</p>
+				<a href="#"><span class="far fa-trash-alt"></span></a>
+			</div>
+			<div>
+				<p class="cart-label">Price</p>
+				<p>$140.00</p>
+			</div>
+		</div><!--cart-item-->
+
+		<div class="cart-item">
+			<img src="assets/bbq_toolset.jpg">
+			<p class="cart-label">Product Name</p>
+			<div>
+				<p class="cart-label">Qty</p>
+				<input type="number" name="quantity" value="1" min="1">
+			</div>
+			<div class="cart-remove">
+				<p class="cart-label">Remove</p>
+				<a href="#"><span class="far fa-trash-alt"></span></a>
+			</div>
+			<div>
+				<p class="cart-label">Price</p>
+				<p>$140.00</p>
+			</div>
+		</div><!--cart-item-->
+	</div><!--cartItems-->
+</div><!--cartSummary-->
+
+<div id="cartCheckout" class="container">
+	<div>
+		<div class="cart-code">
+			<p class="cart-label">Postal Code</p>
+			<input type="text"/>
+			<a href="#" class="btn sec">Enter</a>
+		</div><!--cart-code-->
+		<div class="cart-code">
+			<p class="cart-label">Coupon</p>
+			<input type="text"/>
+			<a href="#" class="btn sec">Enter</a>
+		</div><!--cart-code-->
+	</div>
+
+	<div>
+		<p class="cart-label">Subtotal</p>
+		<p>$280.00</p>
+		<p class="cart-label">Shipping</p>
+		<p>FREE</p>
+		<p class="cart-label">Coupon</p>
+		<p>$0.00</p>
+		<p class="cart-label">Tax</p>
+		<p>$7.26</p>
+		<div class="cart-total">
+			<p>Total</p>
+			<p>$287.26</p>
+		</div><!--cart-total-->
+		<a href="#" class="btn prime">Checkout</a>
+	</div>
+
+
+</div><!--cartCheckout-->
+
---
+++ templates/shipping.php
@@ -0,0 +1,46 @@
+<div id="shipping" class="container">
+	<div id="shippingForm">
+		<h2>Shipping Information</h2>
+		<p>Step 1/3</p>
+		<form>
+			<label>E-mail</label>
+			<input type="email"/>
+			<label>Full Name</label>
+			<input type="text"/>
+			<div>
+				<label>Phone</label>
+				<input type="text" name="phone"/>
+			</div>
+			<div>
+				<label>Alternative Phone</label>
+				<input type="text" name="altphone"/>
+			</div>
+			
+			<label>Address</label>
+			<input type="text"/>
+			<div>
+				<label>Postal/Zip Code</label>
+				<input type="text" name="zipcode"/>
+			</div>
+			<div>
+				<label>Country</label>
+				<select>
+					<option>Select Country</option>
+				</select>
+			</div>
+			<div>
+				<label>State/Province</label>
+				<select>
+					<option>Select State/Province</option>
+				</select>
+			</div>
+			<div>
+				<label>City</label>
+				<select>
+					<option>Select City</option>
+				</select>
+			</div>
+			<a class="btn prime" href="#">Continue to Billing</a>
+		</form>
+	</div><!--shippingForm-->
+</div><!--shipping-->
