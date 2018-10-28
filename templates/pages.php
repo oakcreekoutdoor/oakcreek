09a578a85c810785cf62707ca36f5d7871d849b2
Gabrielle Aoki <GAoki@vanarts.com>
2018-10-27 18:03:39 -0700

    fixed pages template error and moved some views to view folder

--- templates/pages.php
+++ templates/pages.php
@@ -1,9 +1,5 @@
 <?php
 	include("views/header.php");
-<<<<<<< HEAD
 	echo $content;
-=======
-	include("templates/payment.php");
->>>>>>> a206496948e7e97a048deebeeb4fd7cc2b938c9f
 	include("views/footer.php");
 ?>
