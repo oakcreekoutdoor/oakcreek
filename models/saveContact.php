c2d51ee79268377aa6166e4282a77b480e4df109
Gabrielle Aoki <GAoki@vanarts.com>
2018-10-29 20:09:53 -0700

    merge conflict

---
+++ models/saveContact.php
@@ -0,0 +1,22 @@
+<?php 
+include ("../libs/connect.php"); 
+include ("../libs/DBFactory.php"); 
+
+$error = ($_POST["strFullName"] && $_POST["strEmailAddress"] && $_POST["strComment"])?false:true;
+
+$sql = "INSERT INTO contacts (
+		strFullName, 
+		strEmailAddress,
+		strComment
+		) 
+
+	VALUES (
+		'".$_POST["strFullName"]."', 
+		'".$_POST["strEmailAddress"]."',
+		'".$_POST["strComment"]."'
+		
+	)";
+
+
+DBFactory::newData()->runSql("saveData", $sql);
+header("location: ../views/submitthanks.php");?>
