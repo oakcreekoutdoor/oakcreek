5765c8226eb1716d759c4b9046db3e706eb6cc88
Rhea Juarez <RJuarez@vanarts.com>
2018-10-30 23:27:25 -0700

    hamburger menu

--- views/nav.php
+++ views/nav.php
@@ -1,12 +1,15 @@
-
-<nav class="cf">
-	<ul>
-	<?php
-		foreach ($arrData["mainNav"] as $nav) {
-	?>
-		<li><a href="index.php?controller=Pages&action=<?=$nav["strName"]?>"><?=$nav["strName"]?></a></li>
-	<?php
-		}
-	?>
-	</ul>
-</nav><!-- //nav -->
+<section class="container">
+        <label class="hamburger" for="toggle">&#9776;</label>
+		<input type="checkbox" id="toggle"/>
+				<nav class="cf">
+					<ul>
+					<?php
+						foreach ($arrData["mainNav"] as $nav) {
+					?>
+						<li><a href="index.php?controller=Pages&action=<?=$nav["strName"]?>"><?=$nav["strName"]?></a></li>
+					<?php
+						}
+					?>
+					</ul>
+				</nav><!-- //nav -->
+</section>
