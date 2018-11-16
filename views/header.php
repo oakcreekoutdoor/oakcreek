db029aebc72b7e0790a17d1e37794b02b0277f0e
jessicaqiu <jessicaelaineqiu@gmail.com>
2018-11-16 14:54:37 -0800

    Updated CSS and fonts

--- views/header.php
+++ views/header.php
@@ -4,6 +4,9 @@
 	<title>Oak Creek Outdoor Supply</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/style.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:400,600|Muli:400,600,700,700i,800i|Zilla+Slab:700" rel="stylesheet">
+	
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 </head>
 <body>
