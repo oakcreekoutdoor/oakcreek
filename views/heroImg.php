e5897c264c3e09203e753c8997ab7ad385457317
jessicaqiu <jessicaelaineqiu@gmail.com>
2018-11-17 14:11:44 -0800

    Updated hero section and cta button hover

--- views/heroImg.php
+++ views/heroImg.php
@@ -1,5 +1,11 @@
 <section id="heroSection">
 	<div id="heroDiv" class="container">
 		<img src="assets/hero.jpeg" alt="hero" />
+
+		<div class="heroText">
+			<h1>Be fearless.</h1>
+			<p>We have the quality products that you need.</p>
+			<a class="btn prime" href="#">Shop Now</a>
+		</div><!--//heroText-->
 	</div><!-- //section -->
 </section><!-- //heroDiv container -->
