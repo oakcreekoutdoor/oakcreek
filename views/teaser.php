7fefe88256c24699663cb48db604cba442d2429f
jessicaqiu <jessicaelaineqiu@gmail.com>
2018-11-17 18:03:34 -0800

    Added Testimonial teaser section on Homepage

--- views/teaser.php
+++ views/teaser.php
@@ -9,3 +9,18 @@
 		<a class="btn sec" href="#">About Us</a>
 	</div><!--//teaser-grid-->
 </section><!--//teaser-->
+
+<div class="testimonial container">
+	<img src="images/testimonial-bg.png"/>
+
+	<div class="ratings">
+		<img class="leaf" src="images/icon_leaf"/>
+		<img class="leaf" src="images/icon_leaf"/>
+		<img class="leaf" src="images/icon_leaf"/>
+		<img class="leaf" src="images/icon_leaf"/>
+		<img class="leaf" src="images/icon_leaf_beige"/>
+	</div><!--//ratings-->
+
+	<p>“I am extremely pleased with the quality of the rain fly itself, and its ability to keep out wind and rain. I would definitely recommend this product.”</p>
+	<p class="test-name">- Bene Garcia</p>
+</div><!--//testimonial-->
