4964cd81bba9c3879aeac705900de6566f7f1a40
Gabrielle Aoki <GAoki@vanarts.com>
2018-11-17 21:33:28 -0800

    billing and payment layout ajust

--- views/billing.php
+++ views/billing.php
@@ -1,7 +1,7 @@
 <div id="billing" class="container">
 	<div class="shipping-info">
 		<h2>Shipping Information</h2>
-		<!-- <a href="index.php?controller=Cart&action=shipping">Edit</a> -->
+		 <a href="index.php?controller=Cart&action=shipping">Edit</a> 
 		<div>
 			<p><?=$arrData["arrOrder"][0]["strFullName"]?></p>
 			<p><?=$arrData["arrOrder"][0]["strShippingAddress"]?>, <?=$arrData["arrOrder"][0]["strShippingCity"]?></p>
@@ -13,22 +13,22 @@
 		<h2>Billing Information</h2>
 		<p>Step 2/3</p>
 		<form id="billForm" method="post" action="models/saveOrderBill.php">
-			<label>Address</label>
+			<label>Address<span class="req">*</span></label>
 			<input type="text" name="strAddress" id="address" class="requiredField" />
-			<div>
-				<label>Postal/Zip Code</label>
+			<div id="formPostal">
+				<label>Postal/Zip Code<span class="req">*</span></label>
 				<input type="text" name="strZipCode" id="zipcode" class="requiredField" />
 			</div>
 			<div id="formCountry">
-				<label>Country</label>
+				<label>Country<span class="req">*</span></label>
 				<input type="text" name="strCountry" id="country" class="requiredField" />
 			</div><!--formCountry-->
 			<div id="formProvince">
-				<label>State/Province</label>
+				<label>State/Province<span class="req">*</span></label>
 				<input type="text" name="strState" id="state" class="requiredField" />
 			</div><!--formProvince-->
 			<div id="formCity">
-				<label>City</label>
+				<label>City<span class="req">*</span></label>
 				<input type="text" name="strCity" id="city" class="requiredField" />
 			</div><!--formCity-->
 			<input id="billSubmit" type="submit" class="btn prime" value="Continue to Payment" />
