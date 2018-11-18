774b0463db954a869c297119bd32be0fd3a847c0
Gabrielle Aoki <GAoki@vanarts.com>
2018-11-17 23:25:41 -0800

    js toggle active state account nav

--- views/footer.php
+++ views/footer.php
@@ -13,6 +13,7 @@
 <script src="js/submitToShip.js"></script>
 <script src="js/submitToBill.js"></script>
 <script src="js/goToPayment.js"></script>
+<script src="js/toggleAccOptions.js"></script>
 
 </body>
 </html>
