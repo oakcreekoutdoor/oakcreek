f4c5eab2a1f4499273bf3334ee9e663d1c4cac49
jessicaqiu <jessicaelaineqiu@gmail.com>
2018-11-18 16:57:13 -0800

    Hamburger menu works

--- views/nav.php
+++ views/nav.php
@@ -1,31 +1,19 @@
-<nav class="cf" id="hams">
-	<ul>
-	<?php
-		foreach ($arrData["mainNav"] as $nav) {
-	?>
-		<li><a href="index.php?controller=Pages&action=<?=$nav["strName"]?>"><?=$nav["strName"]?></a></li>
-	<?php
-		}
-	?>
-	</ul>
-</nav><!-- //nav -->
+<div class="nav">
+	<label for="toggle">&#9776;</label>
+	<input type="checkbox" id="toggle"/>
+	<nav class="cf" id="hams">
+		<ul>
+		<?php
+			foreach ($arrData["mainNav"] as $nav) {
+		?>
+			<li><a href="index.php?controller=Pages&action=<?=$nav["strName"]?>"><?=$nav["strName"]?></a></li>
+		<?php
+			}
+		?>
+		</ul>
+	</nav>
+</div><!-- //nav -->
 
 <div class="promo">
 	<p>Free Shipping on All Orders (No Minimum)!</p>
 </div><!--//promo-->
-
-<section class="container" id="ham">
-    <label class="hamburger" for="toggle">&#9776;</label>
-	<input type="checkbox" id="toggle"/>
-		<nav class="cf">
-			<ul>
-			<?php
-				foreach ($arrData["mainNav"] as $nav) {
-			?>
-				<li><a href="index.php?controller=Pages&action=<?=$nav["strName"]?>"><?=$nav["strName"]?></a></li>
-			<?php
-				}
-			?>
-			</ul>
-		</nav><!-- //nav -->
-</section>
