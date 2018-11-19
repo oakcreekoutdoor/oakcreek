5320fd93e0d6a666345c6d4969beaf6f2d1c5ad4
Rhea Juarez <RJuarez@vanarts.com>
2018-11-18 23:07:38 -0800

    responsive testimonial page

--- views/testimonial.php
+++ views/testimonial.php
@@ -1,11 +1,39 @@
 <section id="testimonial" class="container cf">
+	<img class="icon" src="images/icon_tent.png"/>
 	<h1><?=$arrData["content"][0]['strHeading1']?></h1>
 	<div class="video">
-		<img src="assets/testimonialvideo1.jpg" alt="testimonail video">
-		<img src="assets/testimonialvideo2.jpg" alt="testimonail video">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/-806_iPyDa4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/Jz46-cMrR_0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>
-	<div class="reviews">
-		<q><?=$arrData["content"][0]['strBodyText']?></q>
-		<p><?=$arrData["content"][0]['strSubheading1']?></p>
+	<div class="testimonial container">
+		<img src="images/testimonial-bg.png"/>
+		<div class="ratings">
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+		</div><!--//ratings according to amazon-->
+		<p class="quote">“ I've still only set it up in my yard, but I spent more time in it and I really like it. It's nice that you can adjust it after the hammock is attached if you need more or less slack to achieve the desired angle.”</p>
+		<p class="test-name">- Tony, 2017</p>
+   </div><!--//testimonial-->
+
+   <div class="video">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/zjWGj0uOz64" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/KfZvk7g3cEw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>
+
+	<div class="testimonial container">
+		<img src="images/testimonial-bg.png"/>
+		<div class="ratings">
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+			<img class="leaf" src="images/icon_leaf"/>
+		</div><!--//ratings according to amazon-->
+
+		<p class="quote">“ So far, so good! It fits well, seems sturdy, shows good quality and has some useful extras, like screened, protected vents on the side and a zippered pouch in front for BBQ tools.”</p>
+		<p class="test-name">- D. Harmon, 2018</p>
+   </div><!--//testimonial-->
 </section><!-- //testimonial -->
