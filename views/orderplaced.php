f0d1afafc8bf2c3ef4c6d6523c7cceff0a56aaae
Gabrielle Aoki <GAoki@vanarts.com>
2018-11-19 23:13:45 -0800

    error msg order thankyou page

--- views/orderplaced.php
+++ views/orderplaced.php
@@ -3,6 +3,14 @@
         <img src="assets/packages.jpg" alt="Picture" />
 	     <h1>Thank You!</h1>
     </div>
+<?php
+    if(isset($_SESSION["orderID"]) && isset($_SESSION['userID']['email'])) {    
+?>
 	<p>Your order number is: <span>#<?=$_SESSION["orderID"]?></span></p>
 	<p>We sent a copy to your e-mail: <span><?=$_SESSION['userID']['email']?></span></p>
+<?php
+     } else {
+        echo "<p>Sorry! We can't find your order. Please contact us!</p>";
+    }
+?>
 </section><!-- //Thank You container -->
